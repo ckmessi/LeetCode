15db8c103558b9dce043dca519bf074b0c23f0c9
ckmessi <ckmessi@163.com>
2017-03-25 09:13:07 +0800

    initial and add problem #217

---
+++ 0217_217. Contains Duplicate/README.md
@@ -0,0 +1,9 @@
+### 217. Contains Duplicate
+
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+* * *
+
+给定一个整数数组，查找是否存在重复元素。   
+三种方案：两两对比，排序后对比相邻，使用哈希表统计。   
+
---
+++ 0217_217. Contains Duplicate/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length <= 1){
+            return false;
+        }
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
